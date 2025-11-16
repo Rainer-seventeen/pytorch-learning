34ea60ae565f5b5b409a17896130d80bffb42b4c
Rainer <rainerseventeen@outlook.com>
2025-11-16 15:29:35 +0800

    feature: dataset edit

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 # 批量读取数据的一个实例，可以通过重写成员函数来实现自定义功能
 
 from torch.utils.data import Dataset
-from PIL import Image
+import cv2
 import os
 
 class Hymenoptera(Dataset):
@@ -28,11 +28,19 @@ class Hymenoptera(Dataset):
             index (int): 数据索引号
 
         Returns:
-            img, label
-        """        
-        img_path = os.path.join(self.path, self.img_list[index]) # 链接路径，获取当前的图片
-        img = Image.open(img_path) # 读取到的PTL格式的图片数据
-        label = self.label_dir # 文件夹名称已经全都对应这个label
+            img (ndarray, RGB), label (str)
+        """
+
+        img_path = os.path.join(self.img_path, self.img_list[index])
+        img = cv2.imread(img_path) # OpenCV 默认 BGR
+
+        # 读取失败检查
+        if img is None:
+            raise FileNotFoundError(f"Failed to read image: {img_path}")
+
+        # 转成 RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        label = self.label_dir
         return img, label
 
 
@@ -46,10 +54,20 @@ class Hymenoptera(Dataset):
 
 
 if __name__ == '__main__':
-    data_root = '/Users/rainer/MyFiles/Code/Learning/Pytorch/dataset/hymenoptera_data'
+    data_root = '/Users/rainer/MyFiles/Code/Learning/Pytorch/dataset/hymenoptera_data/train'
+
     ant_data = Hymenoptera(root=data_root, label='ants')
     bee_data = Hymenoptera(root=data_root, label='bees')
-    train_dataset = ant_data + bee_data # 两个数据集的总和
+
+    train_dataset = ant_data + bee_data
     print(len(ant_data), len(bee_data))
     print(len(train_dataset))
 
+    test_img, test_label = ant_data[3]  # 简写，等同于 __getitem__
+
+    # OpenCV 显示需要 BGR，所以转回来
+    img_bgr = cv2.cvtColor(test_img, cv2.COLOR_RGB2BGR)
+
+    cv2.imshow(test_label, img_bgr)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
